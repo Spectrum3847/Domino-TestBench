6c40a6cbc28ed78f60346839f2adceb3adbe0f9c
WillASmith <washere1999@yahoo.com>
2016-12-30 21:08:26 -0600

    fixed putshorts, upped deadband, added brownout bools, 775 pro toggling

--- src/org/spectrum3847/robot/Dashboard.java
+++ src/org/spectrum3847/robot/Dashboard.java
@@ -1,5 +1,6 @@
 package org.spectrum3847.robot;
 
+import edu.wpi.first.wpilibj.ControllerPower;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import org.spectrum3847.robot.HW;
@@ -12,7 +13,7 @@ public class Dashboard {
 	public static final boolean ENABLE_DASHBOARD = true;
 	
 	
-	static final double SHORT_DELAY = .015;
+	static final double SHORT_DELAY = .005;
     static final double LONG_DELAY = 1;
     
     static double shortOldTime = 0.0;
@@ -24,8 +25,15 @@ public class Dashboard {
     		//SmartDashboard.putBoolean("Relay", false);
     		//SmartDashboard.putNumber("Motor 1", 0);
         	//Robot.shooter.putCompleteOnDashboard();
-    		SmartDashboard.putNumber("Drive Deadband", .1);
+    		SmartDashboard.putNumber("Drive Deadband", .25);
     		SmartDashboard.putBoolean("Squared Drive Inputs", false);
+    		SmartDashboard.putBoolean("775s Enabled", Robot.drive.get775Enabled());
+    		
+    		//Brownout detection - lv 1: 6.8V, drops 6v rail. lv2: 6.3V, drops 5V rail
+    		//True Means SAFE
+    		SmartDashboard.putBoolean("Brownout Stage 1 Status", ControllerPower.getEnabled6V());
+    		SmartDashboard.putBoolean("Brownout Stage 2 Status", ControllerPower.getEnabled5V());
+    		
     		
     		//Battery/Current monitoring
     		SmartDashboard.putNumber("Battery Voltage", HW.PDP.getVoltage());
@@ -47,10 +55,35 @@ public class Dashboard {
     private static void updatePutShort() {
     	//SmartDashboard.putNumber("Motor 1", Motor_1.get());
     	//Robot.shooter.updateValuesToDashboard();
+
+		SmartDashboard.putBoolean("775s Enabled", Robot.drive.get775Enabled());
+		
+		//Brownout detection - lv 1: 6.8V, drops 6v rail. lv2: 6.3V, drops 5V rail
+		//True Means SAFE
+		SmartDashboard.putBoolean("Brownout Stage 1 Status", ControllerPower.getEnabled6V());
+		SmartDashboard.putBoolean("Brownout Stage 2 Status", ControllerPower.getEnabled5V());
+		
+    	
+    	//Battery/Current monitoring
+		SmartDashboard.putNumber("Battery Voltage", HW.PDP.getVoltage());
+		//SmartDashboard.putNumber("Total Current Draw", HW.PDP.getTotalCurrent());
+		//System.out.println(HW.PDP.getTotalCurrent());
+		
+		//Current to each motor
+		SmartDashboard.putNumber("Left First CIM Current", HW.PDP.getCurrent(HW.LEFT_DRIVE_MOTOR_1_PDP));
+		SmartDashboard.putNumber("Left Second CIM Current", HW.PDP.getCurrent(HW.LEFT_DRIVE_MOTOR_2_PDP));
+		SmartDashboard.putNumber("Left 775 Current", HW.PDP.getCurrent(HW.LEFT_DRIVE_MOTOR_3_PDP));
+		SmartDashboard.putNumber("Left Fourth CIM Current", HW.PDP.getCurrent(HW.LEFT_DRIVE_MOTOR_4_PDP));
+		
+		SmartDashboard.putNumber("Right First CIM Current", HW.PDP.getCurrent(HW.RIGHT_DRIVE_MOTOR_1_PDP));
+		SmartDashboard.putNumber("Right Second CIM Current", HW.PDP.getCurrent(HW.RIGHT_DRIVE_MOTOR_2_PDP));
+		SmartDashboard.putNumber("Right 775 Current", HW.PDP.getCurrent(HW.RIGHT_DRIVE_MOTOR_3_PDP));
+		SmartDashboard.putNumber("Right Fourth CIM Current", HW.PDP.getCurrent(HW.RIGHT_DRIVE_MOTOR_4_PDP));
     }
     
     private static void updatePutLong(){
     	//SmartDashboard.putBoolean("Compressor", Compressor.enabled());
+    	//SmartDashboard.putNumber("Total Current Draw", HW.PDP.getTotalCurrent());
     }
 
     
